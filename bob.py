2fac1c1573f57751e237e43ee31ff1f4e62aea06
MrFairbunkle <charliebhfw@icloud.com>
2024-09-01 11:19:06 +0100

    asdfsad

--- bob.py
+++ bob.py
@@ -1,31 +1,48 @@
 import random, os, openai, anvil
 
+# Variables
+#html_file_path = "C:\\Users\\22CfAiRbAnKs-WeStOn.TGS\\Desktop\\A-Level-Repo\\site.html"
+#html_file_path = "E:\Stuff\Things\VSCODE\A-Level-Repo\site.html"
+html_file_path = "site.html"
+css_file_path = "style.css"
+
 # Inputs from user
 site_name = input("What is your site called? ")
 site_sections = input("What sections would you like? Please separate with commas. ")
-while True:
-    preset = input("Would you like to use a preset? Y/N").upper()
-    if preset == "Y":
-        options = input("Professional, ")
-    elif preset == "N":
-        style.css
-        break
 site_col = input("What would you like the main colour to be? ")
 text_col = input("What would you like the text colour to be? ")
 
-# Makes correct number of sections
-sections = site_sections.split(", ")
-print(sections)
-print(len(sections))
-
 # CSS presets
 base = f"""
-
+body {{
+            background-color: {site_col};
+            color: {text_col};
+        }}
+        h1 {{
+            text-align: center;
+        }}
 """
 
 professional = f"""
 
 """
+while True:
+    preset = input("Would you like to use a preset? (Overwrites your colour choices) Y/N ").upper()
+    if preset == "Y":
+        options = input("Professional, ")
+    elif preset == "N":
+        try:
+            with open(css_file_path, 'w') as file:
+                file.write(base)
+            print(f"The content has been saved to {css_file_path}")
+        except Exception as e:
+            print(f"An error occurred while writing to the file: {str(e)}")
+        break
+
+# Makes correct number of sections
+sections = site_sections.split(", ")
+print(sections)
+print(len(sections))
 
 # Basic skeleton
 html = f"""
@@ -36,15 +53,6 @@ html = f"""
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="style.css">
     <title>{site_name}</title>
-    <style>
-        body {{
-            background-color: {site_col};
-            color: {text_col};
-        }}
-        h1 {{
-            text-align: center;
-        }}
-    </style>
 </head>
 <body>
     <h1>{site_name}</h1>
@@ -62,11 +70,6 @@ html += """
 </body>
 </html>
 """
-
-# Create website files
-#html_file_path = "C:\\Users\\22CfAiRbAnKs-WeStOn.TGS\\Desktop\\A-Level-Repo\\site.html"
-#html_file_path = "E:\Stuff\Things\VSCODE\A-Level-Repo\site.html"
-html_file_path = "site.html"
 
 try:
     with open(html_file_path, 'w') as file:
